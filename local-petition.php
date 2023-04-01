3c1b7cd3984b2bd075e6a4895b8d6048ff5f9825
mrsalt <fmark.salisbury@gmail.com>
2023-04-01 15:51:07 -0600

    Add WordPress Plugin Header
    References:
    
    https://developer.wordpress.org/plugins/plugin-basics/
    https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
    https://make.wordpress.org/core/2021/06/29/introducing-update-uri-plugin-header-in-wordpress-5-8/
    https://pear.php.net/package/PhpDocumentor/docs/latest/phpDocumentor/tutorial_tags.package.pkg.html

---
+++ local-petition.php
@@ -0,0 +1,22 @@
+/**
+ * Local Petition
+ *
+ * @package           LocalPetition
+ * @author            Mark Salisbury
+ * @copyright         2023 Citizens for a Library.org
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Local Petition
+ * Plugin URI:        https://github.com/mrsalt/local-petition
+ * Description:       Local Petition is a WordPress plugin which allows you to create a petition which shows where signers live (but does not identify them by name on a map).
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            Mark Salisbury
+ * Author URI:        https://github.com/mrsalt/me
+ * Text Domain:       local-petition
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:        https://github.com/mrsalt/local-petition
+ */
