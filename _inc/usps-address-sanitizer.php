3ac317cbe470cb5d52472045a41ffa19ab512e9e
Mark Salisbury <fmark.salisbury@gmail.com>
2025-09-04 22:35:59 -0600

    Switch to new USPS APIs

--- _inc/usps-address-sanitizer.php
+++ _inc/usps-address-sanitizer.php
@@ -1,78 +1,122 @@
 <?php
 
-function xml_escape($text)
+// Make a request to $url with $params (associative array).
+function make_request($method, $url, $params, $headers = array())
 {
-    return htmlentities($text, ENT_QUOTES | ENT_SUBSTITUTE | ENT_XML1);
+    $ch = curl_init();
+    $curl_opts = [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_URL => $url,
+        CURLOPT_CUSTOMREQUEST => $method,
+        CURLOPT_POSTFIELDS => http_build_query($params)
+    ];
+    if (!empty($headers)) {
+        $formatted_headers = array();
+        foreach ($headers as $key => $value) {
+            $formatted_headers[] = $key . ': ' . $value;
+        }
+        $curl_opts[CURLOPT_HTTPHEADER] = $formatted_headers;
+    }
+    if ($method === 'GET') {
+        $curl_opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+    } else {
+        $curl_opts[CURLOPT_POSTFIELDS] = http_build_query($params);
+    }
+    curl_setopt_array($ch, $curl_opts);
+    $result = curl_exec($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if ($result === false) {
+        throw new Exception('curl_exec() returned false: curl_error=' . curl_error($ch) . ', curl_get_info=' . var_export(curl_getinfo($ch), true));
+    }
+    return array(
+        'http_code' => $http_code,
+        'response' => $result
+    );
 }
 
-function simple_xml_parse($xml, $element, $required = true)
+function check_result($result)
 {
-    $start_tag = '<' . $element . '>';
-    $start_pos = strpos($xml, $start_tag);
-    if ($start_pos == false) {
-        if (!$required)
-            return null;
-        throw new Exception("Unable to find $start_tag in $xml");
+    if ($result['http_code'] < 200 || $result['http_code'] >= 300) {
+        throw new Exception('Request failed with HTTP code ' . $result['http_code'] . ', result = ' . var_export($result['response'], true));
     }
-    $end_tag = '</' . $element . '>';
-    $end_pos = strpos($xml, $end_tag, $start_pos);
-    if ($end_pos == false)
-        throw new Exception("Unable to find $end_tag in $xml");
-    $start_pos += strlen($start_tag);
-    $value = substr($xml, $start_pos, $end_pos - $start_pos);
-    return html_entity_decode($value, ENT_QUOTES | ENT_SUBSTITUTE | ENT_XML1);
+    return $result['response'];
+}
+
+function get_oauth_token()
+{
+    $temp_dir = sys_get_temp_dir();
+    $oauth_file = $temp_dir . DIRECTORY_SEPARATOR . 'usps_oauth.json';
+    // Get current time in epoch format
+    $now = time();
+
+    if (file_exists($oauth_file)) {
+        $json_str = file_get_contents($oauth_file);
+        $oauth_obj = json_decode($json_str, true); // associative array
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Error parsing usps_oauth.json: ' . json_last_error_msg());
+        }
+
+        if (isset($oauth_obj['issue_time'])) {
+            // If issue_time is less than 7 hours ago, it should still be good.  Technically
+            // it's supposed to be good for 8 hours.
+            if ($oauth_obj['issue_time'] < ($now + 7 * 3600)) {
+                return $oauth_obj['access_token'];
+            }
+        }
+    }
+
+    $usps_consumer_key = get_cfg_var('usps_consumer_key');
+    if (!$usps_consumer_key)
+        throw new Exception('No usps_consumer_key set in php.ini');
+    $usps_consumer_secret = get_cfg_var('usps_consumer_secret');
+    if (!$usps_consumer_secret)
+        throw new Exception('No usps_consumer_secret set in php.ini');
+
+    $payload = array(
+        'grant_type' => 'client_credentials',
+        'client_id' => $usps_consumer_key,
+        'client_secret' => $usps_consumer_secret,
+        'scope' => 'addresses'
+    );
+
+    $url = 'https://apis.usps.com/oauth2/v3/token';
+    $response = make_request('POST', $url, $payload, array('Content-Type' => 'application/x-www-form-urlencoded'));
+    $response = check_result($response);
+    $obj = json_decode($response, true); // associative array
+
+    $json_str = json_encode(array(
+        'access_token' => $obj,
+        'issue_time' => $now
+    ));
+    file_put_contents($oauth_file, $json_str);
+    return $obj;
 }
 
 function sanitize_address($address)
 {
-    $user_id = get_cfg_var('usps_api_user_id');
-    if (!$user_id)
-        throw new Exception('No usps_api_user_id set in php.ini');
-
-    // USPS does not document their API very well, but from results returned, Address2
-    // is the 'primary' line of the address.  Apartment Number, etc. can be in Address1
-    // but technically should be at the end of Address2.  Address1 is intended for C/O
-    // (care of).
-    // This is why I'm swapping line1/line2 here.
-    $body = '<AddressValidateRequest USERID="' . $user_id . '">' .
-        '<Address ID="0">' .
-        '<Address1>' . xml_escape($address['line_2']) . '</Address1>' .
-        '<Address2>' . xml_escape($address['line_1']) . '</Address2>' .
-        '<City>' . xml_escape($address['city']) . '</City>' .
-        '<State>' . xml_escape($address['state']) . '</State>' .
-        '<Zip5>' . xml_escape($address['zip']) . '</Zip5>' .
-        '<Zip4></Zip4>' .
-        '</Address>' .
-        '</AddressValidateRequest>';
-
-    $ch = curl_init();
-    curl_setopt_array($ch, [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => 'https://production.shippingapis.com/ShippingAPI.dll',
-        CURLOPT_POST => 1,
-        CURLOPT_POSTFIELDS => http_build_query(array('API' => 'Verify', 'XML' => $body))
-    ]);
-    $result = curl_exec($ch);
-    if ($result === false) {
-        throw new Exception('curl_exec() returned false: curl_error=' . curl_error($ch) . ', curl_get_info=' . var_export(curl_getinfo($ch), true));
+    $usps_oauth = get_oauth_token();
+    $url = 'https://apis.usps.com/addresses/v3/address';
+    $params = array(
+        'streetAddress' => $address['line_1'],
+        'secondaryAddress' => $address['line_2'],
+        'city' => $address['city'],
+        'state' => $address['state'],
+        'ZIPCode' => $address['zip']
+    );
+    $result = make_request('GET', $url, $params, $headers = array('Authorization' => 'Bearer ' . $usps_oauth['access_token'], 'Content-Type' => 'application/x-www-form-urlencoded'));
+    $response = json_decode($result['response'], true); // associative array
+    if ($result['http_code'] != 200) {
+        return array('Error' => $response['error']['message']);
     }
-    curl_close($ch);
-
-    if (strpos($result, '<Error>')) {
-        return array('Error' => simple_xml_parse($result, 'Description'));
-        //return '<pre>'.var_export(Array('Error' => simple_xml_parse($result, 'Description')), true).'</pre>';
-    }
-
-    //<AddressValidateResponse><Address ID="0"><Error><Number>-2147219401</Number><Source>clsAMS</Source><Description>Address Not Found.  </Description><HelpFile/><HelpContext/></Error></Address></AddressValidateResponse>
-    //<AddressValidateResponse><Address ID="0"><Address1>APT G406</Address1><Address2>3400 E RIVER VALLEY ST</Address2><City>MERIDIAN</City><State>ID</State><Zip5>83646</Zip5><Zip4>2312</Zip4></Address></AddressValidateResponse>
-    //return '<pre>'.htmlentities($result).'</pre>'.'<pre>Address2 = '.simple_xml_parse($result, 'Address2').'</pre>';
+    $address = $response['address'];
     return array(
-        'line_1' => simple_xml_parse($result, 'Address2'),
-        'line_2' => simple_xml_parse($result, 'Address1', false),
-        'city' => simple_xml_parse($result, 'City'),
-        'state' => simple_xml_parse($result, 'State'),
-        'zip' => simple_xml_parse($result, 'Zip5'),
-        'zip_ext' => simple_xml_parse($result, 'Zip4', false)
+        'line_1' => $address['streetAddress'],
+        'line_2' => $address['secondaryAddress'],
+        'city' => $address['city'],
+        'state' => $address['state'],
+        'zip' => $address['ZIPCode'],
+        'zip_ext' => $address['ZIPPlus4']
     );
 }
 
@@ -131,7 +175,8 @@ function update_coordinates($address_id, $coordinates)
 // 13319 W Silverbrook Dr, Apt B, Boise, ID 83713
 // 5 parts:
 // 13319 W Silverbrook Dr, Apt B, Boise, ID 83713, USA
-function parse_address_with_commas($formatted_address) {
+function parse_address_with_commas($formatted_address)
+{
     $parts = explode(', ', $formatted_address);
     if (count($parts) == 3 || count($parts) == 4)
         $zip_part = 2;
@@ -144,8 +189,7 @@ function parse_address_with_commas($formatted_address) {
     if ($sep !== false) {
         $state = substr($parts[$zip_part], 0, $sep);
         $zip = substr($parts[$zip_part], $sep + 1);
-    }
-    else {
+    } else {
         $state = $parts[$zip_part];
         $zip = '';
     }
@@ -155,5 +199,6 @@ function parse_address_with_commas($formatted_address) {
         'line_2' => count($parts) == 5 ? strtoupper($parts[1]) : null,
         'city' => $city,
         'state' => $state,
-        'zip' => $zip);
+        'zip' => $zip
+    );
 }
